490dfa28e1a173e673efca0729b1e7f7879e31ff
ArtKuzmin <111068507+ArtKuzmin@users.noreply.github.com>
2022-10-16 15:01:08 +0700

    Delete s2test_2.py

--- s2test_2.py
+++
@@ -1,8 +0,0 @@
-# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
-N = (int(input("Input number: ")))
-sum = 1
-lst = []
-for x in range(1, N+1):
-    sum *= x  
-    lst.append(sum)
-print(lst)
